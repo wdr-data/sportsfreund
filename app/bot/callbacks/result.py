c9e3d4bbd148266cbd3f21103ab1335374cfb61f
LizzAy <lisachenbach@gmail.com>
2017-11-22 18:12:56 +0100

    adding feed-information to answere the podium question - first version

--- app/bot/callbacks/result.py
+++ app/bot/callbacks/result.py
@@ -1,17 +1,32 @@
-
 from ..fb import send_text
+from feeds.models.match import Match
 
-def api_winner(event, parameters,**kwargs):
+import logging
+logger = logging.Logger(__name__)
+
+match = Match()
+
+
+def api_winner(event, parameters, **kwargs):
     sender_id = event['sender']['id']
     sport = parameters.get('sport')
     discipline = parameters.get('discipline')
 
-    send_text(sender_id, 'Hier gibt es bald den gewinner des' + sport + ' '+ discipline)
+    send_text(sender_id, 'Hier gibt es bald den Gewinner des ' + sport + ' '+ discipline)
 
 
-def api_podium(event,parameters,**kwargs):
+def api_podium(event, parameters, **kwargs):
     sender_id = event['sender']['id']
     sport = parameters.get('sport')
-    discipline = parameters.get('discipline')
+    #discipline = parameters.get('discipline')
 
-    send_text(sender_id, 'Hier gibt es bald das Podium des' + sport + ' ' + discipline)
+    asked_match = match.by_id(8471268)
+    podium = []
+
+    discipline = asked_match['round']['name']
+    results = asked_match['match_result']
+    for result in results:
+        if int(result['rank']) <= 3 and int(result['match_result_at']) == 0:
+            podium.append(result['team_id'])
+
+    send_text(sender_id, 'Im ' +sport + ' haben in der Disziplin ' + discipline + ' folgende Atheten die ersten drei Plätze belegt:\n' + str(podium))
