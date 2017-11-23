a97435b3f33e3e19dd07d5ed82b89132de989caf
LizzAy <lisachenbach@gmail.com>
2017-11-23 17:55:57 +0100

    broken function api_podium to test

--- app/bot/callbacks/result.py
+++ app/bot/callbacks/result.py
@@ -1,14 +1,11 @@
 import logging
-from datetime import datetime
 
 from ..fb import send_text
 from feeds.models.match import Match
 from feeds.models.team import Team
+from feeds.models.match_meta import MatchMeta
 
 logger = logging.Logger(__name__)
-
-match = Match()
-team = Team()
 
 OFFSET = 127462 - ord('A')
 
@@ -16,49 +13,74 @@ OFFSET = 127462 - ord('A')
 def flag(code):
     return chr(ord(code[0]) + OFFSET) + chr(ord(code[1]) + OFFSET)
 
+
 def api_winner(event, parameters, **kwargs):
     sender_id = event['sender']['id']
     sport = parameters.get('sport')
     discipline = parameters.get('discipline')
 
-    send_text(sender_id, 'Hier gibt es bald den Gewinner des ' + sport + ' '+ discipline)
+    match_id = MatchMeta.search_last(discipline=discipline or None, sport=sport or None)
+    asked_match = Match.by_id(match_id)
+
+    if discipline is None:
+        discipline = asked_match.round.name
+
+    if asked_match.finnished == 'yes':
+        results = asked_match.match_result
+
+        winner_teams = [Team.by_id(winner.team_id) for winner in results[:3]]
+
+        send_text(sender_id,
+                  '{winner} hat das {sport} Rennen in der Disziplin {discipline} '
+                  'in {city}, {country} am {date} gewonnen.'.format(
+                      winner=' '.join([winner_teams[0].name,
+                                       flag(winner_teams[0].country.iso),
+                                       winner_teams[0].country.code]),
+                      sport=sport,
+                      discipline=discipline,
+                      city=asked_match.venue.town.name,
+                      country=asked_match.venue.country.name,
+                      date=asked_match.date.strftime('%d.%m.%Y'),
+
+                  ))
+    else:
+        send_text(sender_id,
+                  'Das Event wurde noch nicht beendet. Frage später erneut.')
 
 
 def api_podium(event, parameters, **kwargs):
     sender_id = event['sender']['id']
     sport = parameters.get('sport')
-    #discipline = parameters.get('discipline')
+    discipline = parameters.get('discipline')
 
-    asked_match = match.by_id(8471268)
-    podium = []
+    match_meta = MatchMeta.search_last(discipline=discipline or None, sport=sport or None)
+    logger.debug('Match ID looking for: ' + str(match_meta.id))
+    asked_match = Match.by_id(match_meta.id)
 
-    discipline = asked_match['round']['name']
-    results = asked_match['match_result']
-    podium = results[:3]
-    winner_teams = []
+    if discipline is None:
+        discipline = asked_match.round.name
 
-    date = datetime.strptime(asked_match['match_date'], '%Y-%m-%d')
+    if asked_match.finished == 'yes':
+        results = asked_match.match_result
+        winner_teams = [Team.by_id(winner.team_id) for winner in results[:3]]
 
-    for winner in podium:
-        winner_teams.append(team.by_id(winner['team_id']))
-
-    logger.info('Found winner as follows '+str(winner_teams[0]['name'])+' '+str(winner_teams[1]['name'])+' '+str(winner_teams[2]['name']))
-
-    send_text(sender_id,
-              'Ergebnis beim {sport} {discipline} in {city}, {country} am {date}:\n' \
-                '1. {winner_1}\n2. {winner_2}\n3. {winner_3}'.format(
-                  sport = sport,
-                  discipline = discipline,
-                  city = asked_match['venue']['town']['name'],
-                  country = asked_match['venue']['country']['name'],
-                  date = date.strftime('%d.%m.%Y'),
-                  winner_1 = ' '.join([winner_teams[0]['name'],
-                                      flag(winner_teams[0]['country']['iso']),
-                                      winner_teams[0]['country']['code']]),
-                  winner_2 = ' '.join([winner_teams[1]['name'],
-                                      flag(winner_teams[1]['country']['iso']),
-                                      winner_teams[1]['country']['code']]),
-                  winner_3 = ' '.join([winner_teams[2]['name'],
-                                      flag(winner_teams[2]['country']['iso']),
-                                      winner_teams[2]['country']['code']]),
-              ))
+        send_text(sender_id,
+                  'Ergebnis beim {discipline} in {city}, {country} am {date}:\n'
+                  '1. {winner_1}\n2. {winner_2}\n3. {winner_3}'.format(
+                      discipline=discipline,
+                      city=asked_match.venue.town.name,
+                      country=asked_match.venue.country.name,
+                      date=asked_match.date.strftime('%d.%m.%Y'),
+                      winner_1=' '.join([winner_teams[0].name,
+                                        flag(winner_teams[0].country.iso),
+                                        winner_teams[0].country.code]),
+                      winner_2=' '.join([winner_teams[1].name,
+                                        flag(winner_teams[1].country.iso),
+                                        winner_teams[1].country.code]),
+                      winner_3=' '.join([winner_teams[2].name,
+                                        flag(winner_teams[2].country.iso),
+                                        winner_teams[2].country.code]),
+                  ))
+    else:
+        send_text(sender_id,
+                  'Das Event wurde noch nicht beendet. Frage später erneut.')
