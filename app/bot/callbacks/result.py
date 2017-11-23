e6e8cd6ae539641ed1bd4fecf8be62460444e426
LizzAy <lisachenbach@gmail.com>
2017-11-23 13:36:16 +0100

    changed podium func to show winners and infos

--- app/bot/callbacks/result.py
+++ app/bot/callbacks/result.py
@@ -1,11 +1,20 @@
+import logging
+from datetime import datetime
+
 from ..fb import send_text
 from feeds.models.match import Match
+from feeds.models.team import Team
 
-import logging
 logger = logging.Logger(__name__)
 
 match = Match()
+team = Team()
 
+OFFSET = 127462 - ord('A')
+
+
+def flag(code):
+    return chr(ord(code[0]) + OFFSET) + chr(ord(code[1]) + OFFSET)
 
 def api_winner(event, parameters, **kwargs):
     sender_id = event['sender']['id']
@@ -25,8 +34,31 @@ def api_podium(event, parameters, **kwargs):
 
     discipline = asked_match['round']['name']
     results = asked_match['match_result']
-    for result in results:
-        if int(result['rank']) <= 3 and int(result['match_result_at']) == 0:
-            podium.append(result['team_id'])
+    podium = results[:3]
+    winner_teams = []
 
-    send_text(sender_id, 'Im ' +sport + ' haben in der Disziplin ' + discipline + ' folgende Atheten die ersten drei Plätze belegt:\n' + str(podium))
+    date = datetime.strptime(asked_match['match_date'], '%Y-%m-%d')
+
+    for winner in podium:
+        winner_teams.append(team.by_id(winner['team_id']))
+
+    logger.info('Found winner as follows '+str(winner_teams[0]['name'])+' '+str(winner_teams[1]['name'])+' '+str(winner_teams[2]['name']))
+
+    send_text(sender_id,
+              'Ergebnis beim {sport} {discipline} in {city}, {country} am {date}:\n' \
+                '1. {winner_1}\n2. {winner_2}\n3. {winner_3}'.format(
+                  sport = sport,
+                  discipline = discipline,
+                  city = asked_match['venue']['town']['name'],
+                  country = asked_match['venue']['country']['name'],
+                  date = date.strftime('%d.%m.%Y'),
+                  winner_1 = ' '.join([winner_teams[0]['name'],
+                                      flag(winner_teams[0]['country']['iso']),
+                                      winner_teams[0]['country']['code']]),
+                  winner_2 = ' '.join([winner_teams[1]['name'],
+                                      flag(winner_teams[1]['country']['iso']),
+                                      winner_teams[1]['country']['code']]),
+                  winner_3 = ' '.join([winner_teams[2]['name'],
+                                      flag(winner_teams[2]['country']['iso']),
+                                      winner_teams[2]['country']['code']]),
+              ))
