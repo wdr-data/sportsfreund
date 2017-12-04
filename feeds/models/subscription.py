7ed9aa29c748bfd6bbc275311917b0532da46c85
ChristianJoe <cjoe@gmx.de>
2017-12-04 18:24:42 +0100

    :bug: Avoid usage of builtin descriptors

--- feeds/models/subscription.py
+++ feeds/models/subscription.py
@@ -15,22 +15,22 @@ class Subscription(Model):
         RESULT = 'result'
 
     @classmethod
-    def create(cls, psid, target, filter, type):
+    def create(cls, psid, target, filter_arg, type_arg):
 
         if target not in cls.Target:
             raise ValueError(f'invalid target: {target}')
 
-        if type not in cls.Type:
-            raise ValueError(f'invalid type: {type}')
+        if type_arg not in cls.Type:
+            raise ValueError(f'invalid type: {type_arg}')
 
-        if not isinstance(filter,dict):
-            raise ValueError(f'filter should be dict: {filter}')
+        if not isinstance(filter_arg, dict):
+            raise ValueError(f'filter should be dict: {filter_arg}')
 
         data = {
-            psid: psid,
-            target: target,
-            filter: filter,
-            type: type,
+            'psid': psid,
+            'target': target.value,
+            'filter': filter_arg,
+            'type': type_arg.value,
         }
 
         cls.collection.replace_one(data, data, upsert=True)
