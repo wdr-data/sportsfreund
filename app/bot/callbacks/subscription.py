b4309a3cc925df9ab5869a98f6ae3a4ef68873ef
LizzAy <lisachenbach@gmail.com>
2017-12-05 17:20:04 +0100

    :sparkles: Allow user to unsubscribe from result pushes

--- app/bot/callbacks/subscription.py
+++ app/bot/callbacks/subscription.py
@@ -1,4 +1,5 @@
-from ..fb import send_text, send_list, list_element, button_postback
+from ..handlers.payloadhandler import PayloadHandler
+from ..fb import send_text, send_list, list_element, button_postback, send_buttons
 from ..handlers.apiaihandler import ApiAiHandler
 from feeds.models.subscription import Subscription
 
@@ -12,10 +13,10 @@ def api_subscribe(event, parameters, **kwargs):
         send_text(sender_id,
                   'Magst du dich für Biathlon oder Ski-Alpin Ergebnisse anmelden?')
         return
-    subscribe(event, sport, discipline)
+    subscribe_flow(event, sport, discipline)
 
 
-def subscribe(event, sport, discipline=None):
+def subscribe_flow(event, sport, discipline=None):
     sender_id = event['sender']['id']
 
     target = Subscription.Target.SPORT if sport else Subscription.Target.DISCIPLINE
@@ -36,9 +37,10 @@ def subscribe(event, sport, discipline=None):
 
 
 def api_unsubscribe(event, parameters, **kwargs):
-    unsubscribe(event)
+    unsubscribe_flow(event)
 
-def unsubscribe(event):
+
+def unsubscribe_flow(event):
     sender_id = event['sender']['id']
     send_text(sender_id,
               'Du möchtest dich von automatischen Nachrichten abmelden - OK. '
@@ -53,10 +55,12 @@ def send_subscriptions(event, **kwargs):
 
     if any(sub.target is Subscription.Target.HIGHLIGHT for sub in subs):
         highlight_emoji, highlight_button = '✔', button_postback('Abmelden',
-                                                                 ['highlight_subscribe'])
+                                                                 {'target': 'highlight',
+                                                                  'state': 'unsubscribe'})
     else:
         highlight_emoji, highlight_button = '❌', button_postback('Anmelden',
-                                                                 ['highlight_unsubscribe'])
+                                                                 {'target': 'highlight',
+                                                                  'state': 'subscribe'})
 
     if any(sub.type is Subscription.Type.RESULT for sub in subs):
         result_subtitle = ', '.join(
@@ -79,7 +83,7 @@ def send_subscriptions(event, **kwargs):
         list_element(
             'Ergebnisdienst ' + result_emoji,
             result_subtitle,
-            buttons=[button_postback('🔧 Ändern', ['result_subscriptions'])])
+            buttons=[button_postback('🔧 Ändern', {'type': 'result'})])
     ]
 
     send_list(
@@ -88,7 +92,61 @@ def send_subscriptions(event, **kwargs):
     )
 
 
+def manage_subscriptions(event, payload, **kwargs):
+    sender_id = event['sender']['id']
+    state = payload['state']
+    target = payload['target']
+
+    if target == 'highlight':
+        if state == 'subscribe':
+            send_text(sender_id,
+                      'Diese Funktion ist erst zu den Olympischen Winterspielen verfügbar.')
+        elif state == 'unsubscribe':
+            send_text(sender_id,
+                      'Diese Funktion ist erst zu den Olympischen Winterspielen verfügbar.')
+
+
+def change_subscription(event, payload, **kwargs):
+    sender_id = event['sender']['id']
+    type = payload['type']
+    offset = payload.get('offset', 0)
+    subs = Subscription.query(psid=sender_id, type=type)
+
+    if len(subs) == 0:
+        send_text(sender_id, 'Du bist noch für keinen Nachrichten Dienst angemeldet.')
+        return
+    elif len(subs) == 1:
+        send_buttons(sender_id,
+                     f'Du bist für den Nachrichten Dienst {type} zum Thema '
+                     f'{Subscription.describe_filter(subs[0].filter)} angemeldet. '
+                     f'Möchtest du dich abmelden?',
+                     button_postback('Abmelden', ['unsubscribe']))
+        return
+
+    num_subs = 4
+    if len(subs) - (offset + num_subs) == 1:
+        num_subs = 3
+
+    elements = [
+        list_element(Subscription.describe_filter(sub.filter),
+                     button_postback('Abmelden', {'unsubscribe': str(sub._id)}))
+        for sub in subs[offset:offset + num_subs]
+    ]
+
+    if len(subs) - offset > num_subs:
+        new_payload = payload.copy()
+        new_payload['offset'] = offset + num_subs
+        button = button_postback('Mehr anzeigen', new_payload)
+    else:
+        button = None
+
+    send_list(sender_id, elements, button=button)
+
+
+
 handlers = [
-    ApiAiHandler(api_subscribe, 'push.subscription.subscribe', follow_up=True)
-    ApiAiHandler(api_subscribe, 'push.subscription.subscribe')
+    ApiAiHandler(api_subscribe, 'push.subscription.subscribe', follow_up=True),
+    ApiAiHandler(api_subscribe, 'push.subscription.subscribe'),
+    PayloadHandler(manage_subscriptions, ['target', 'state']),
+    PayloadHandler(change_subscriptions, ['type'])
 ]
