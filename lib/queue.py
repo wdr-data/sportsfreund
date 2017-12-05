43c78f347f1cf8e812c86d53ebb053e42a3198e1
Marcus Weiner <marcus.weiner@gmail.com>
2017-12-05 19:24:13 +0100

    :ambulance: Fix mrq config import on Heroku

--- lib/queue.py
+++ lib/queue.py
@@ -2,5 +2,15 @@ import os
 from mrq import context, config
 from mrq.job import queue_job
 
-cfg = config.get_config(file_path="/mrq-config.py")
+config_locations = ["../worker/mrq-config.py", "./worker/mrq-config.py", "/mrq-config.py"]
+file_path = None
+
+for path in config_locations:
+    if os.path.isfile(path):
+        file_path = path
+
+if file_path is None:
+    raise RuntimeError("MRQ Config not found!")
+
+cfg = config.get_config(file_path=file_path)
 context.set_current_config(cfg)
