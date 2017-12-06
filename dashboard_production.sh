6036beb6fc5fe65892a679d65d5a29e40c83788b
Marcus Weiner <marcus.weiner@gmail.com>
2017-12-06 19:02:16 +0100

    :wrench: Improve production dashboard launch

--- dashboard_production.sh
+++ dashboard_production.sh
@@ -3,5 +3,6 @@
 MONGODB_URI=`heroku config:get MONGODB_URI`
 REDIS_URL=`heroku config:get REDIS_URL`
 
-docker run -p 5556:5555 -e MONGODB_URI=${MONGODB_URI} -e REDIS_URL=${REDIS_URL} \
+docker-compose build worker_dashboard
+docker run -it -p 127.0.0.1:5556:5555 -e MONGODB_URI=${MONGODB_URI} -e REDIS_URL=${REDIS_URL} \
        sportsfreund_worker_dashboard mrq-dashboard
