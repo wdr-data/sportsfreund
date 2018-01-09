7896d177d03b886b216ef794ee495df447b60b19
ChristianJoe <cjoe@gmx.de>
2018-01-09 14:43:08 +0100

    :bug: Use absolute path

--- feeds/config/__init__.py
+++ feeds/config/__init__.py
@@ -1,7 +1,12 @@
+import os
+from pathlib import Path
+
 import yaml
 
-with open('discipline_aliases.yml', 'r') as f:
+BASE_DIR = Path(os.path.dirname(os.path.abspath(__file__)))
+
+with open(BASE_DIR / 'discipline_aliases.yml', 'r') as f:
     DISCIPLINE_ALIASES = yaml.load(f)
 
-with open('sports_config.yml', 'r') as f:
+with open(BASE_DIR / 'sports_config.yml', 'r') as f:
     SPORTS_CONFIG = yaml.load(f)
