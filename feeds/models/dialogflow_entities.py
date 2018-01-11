6e142fa505844fc68e02e82bd5595791d052f93b
ChristianJoe <cjoe@gmx.de>
2018-01-11 19:47:46 +0100

    :sparkles: Add olympic feed to calendar

---
+++ feeds/models/dialogflow_entities.py
@@ -0,0 +1,26 @@
+
+"""
+Basic tests for models and caching.
+
+You can paste this file into a python shell you start with
+docker-compose run web python
+"""
+
+from feeds.models.match_meta import MatchMeta
+import re
+
+s = MatchMeta.search_range()
+t = str(s)
+
+item_list = list(sorted(set(re.findall(r'"discipline_short": "(.*?)"', t.replace("'",'"'), re.DOTALL))))
+
+
+item_list = list(sorted(set(re.findall(r'"discipline": {[^}]*?"name": "(.*?)"', t.replace("'",'"'), re.DOTALL))))
+
+entities = []
+for item in item_list:
+    entities.append({
+        "value": item,
+        "synonyms": [item]
+    })
+
--- feeds/models/match_meta.py
+++ feeds/models/match_meta.py
@@ -117,13 +117,94 @@ class MatchMeta(FeedModel):
         return True
 
     @classmethod
+    def load_olympia_feed(cls, id, clear_cache=False):
+        """
+        Load all items in a matches-by-topic-for-season feed if its cache is expired
+
+        :param id: The topic ID
+        :param clear_cache: If `True`, the cache for the feed is cleared and it is reloaded
+            and cached from the feed. Default is `False`
+
+        :returns `True` if the feed has been updated, else `False` (cache hit)
+        """
+        id = str(id)
+
+        cls.logger.info('%s olympic match metas in db', cls.collection.count())
+
+        cache_marker = cls.collection.find_one({'_feed_id': id})
+
+        if cache_marker:
+            cls.logger.debug('Cache hit for %s with id %s', cls.__name__, id)
+
+            if clear_cache:
+                cls.logger.debug('Force-clear cache for %s with id %s', cls.__name__, id)
+
+            elif cache_marker['_cached_at'] + cls.cache_time < time():
+                cls.logger.debug('Cache expired for %s with id %s', cls.__name__, id)
+
+            else:
+                return False
+
+        obj = cls.api_function(**{cls.api_id_name: id})
+
+        now = int(time())
+
+        for sp in obj:
+            for co in sp['competition']:
+                for se in co['season']:
+                    for ro in se['round']:
+                        for ma in ro['match']:
+                            ma['sport'] = sp['name']
+                            ma['sport_id'] = sp['id']
+                            ma['season'] = se['name']
+                            ma['season_id'] = se['id']
+                            ma['competition'] = ro['name']
+                            ma['competition_id'] = ro['id']
+                            ma['gender'] = co['gender']
+                            ma['discipline'] = co['name']
+                            ma['discipline_short'] = co['shortname']
+                            ma['_cached_at'] = now
+                            ma['match_incident'] = ma.get('match_incident')
+
+                            if ma['match_time'] == 'unknown':
+                                ma['match_time'] = 'unbekannt'
+                                datetime_str = '%s %s' % (ma['match_date'], '23:59')
+                            else:
+                                datetime_str = '%s %s' % (ma['match_date'], ma['match_time'])
+
+                            ma['datetime'] = datetime.strptime(
+                                datetime_str,
+                                '%Y-%m-%d %H:%M'
+                            )
+
+                            cls.collection.replace_one({'id': ma['id']}, ma, upsert=True)
+
+        cls.logger.info('%s match metas in db', cls.collection.count())
+
+        new_cache_marker = {'_feed_id': id, '_cached_at': now}
+
+        if cache_marker:
+            cls.collection.replace_one({'_id': cache_marker['_id']}, new_cache_marker)
+
+        else:
+            cls.collection.insert_one(new_cache_marker)
+
+        return True
+
+    @classmethod
     def _search(cls, base_filter, sport, discipline, town, country, gender):
 
         if sport is not None:
-            id = sport_by_name[sport].topic_id
-            cls.load_feed(id)
+            try:
+                id = sport_by_name[sport].topic_id
+                cls.load_feed(id)
+            except:
+                pass
+
         else:
             cls.logger.warning('TODO check feeds with only discipline')
+
+        cls.load_olympia_feed(1757)
 
         filter = base_filter.copy()
 
