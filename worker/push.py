0a767edcdced41ed0c0a257b6c81316b2abd3e95
ChristianJoe <cjoe@gmx.de>
2018-01-29 15:01:47 +0100

    :fire: Delete text before result push

--- worker/push.py
+++ worker/push.py
@@ -16,14 +16,6 @@ from lib import queue
 
 MATCH_CHECK_INTERVAL = 60
 
-RESULT_PUSH_INTRO = [
-    'Und wieder ging ein Event zu Ende. Hier die Ergebnisse:',
-    "Ein Wettkampf wurde beendet und ich sag dir wer gewonnen hat:",
-    'Schau an. Es wurden mal wieder Höchstleistungen vollbracht:',
-    'Hier deine bestellten Ergebnisse.',
-    'Es war mal wieder Aktion angesagt. Hier die Ergebnisse vom letzten Wettstreit:',
-    'Nicht erschrecken. Es wurde nur mal wieder ein Wettkampf beendet.',
-]
 
 class UpdateSchedule(Task):
 
@@ -111,7 +103,6 @@ class UpdateMatch(Task):
 
         for uid in podium_ids:
             event = Replyable({'sender': {'id': uid}}, type=SenderTypes.FACEBOOK)
-            event.send_text(random.choice(RESULT_PUSH_INTRO))
             event.send_list(match.lst_podium, top_element_style='large', button=match.btn_podium)
 
         for uid, sub in zip(athlete_ids, athlete_subs):
@@ -124,7 +115,7 @@ class UpdateMatch(Task):
                 f'{points} Punkten'
 
             event.send_text(f'{meta.sport} {meta.discipline} in {meta.town} wurde soeben beendet. '
-                       f'Wollen wir mal sehen, wie {athlete} abgeschnitten hat...')
+                            f'Wollen wir mal sehen, wie {athlete} abgeschnitten hat...')
             event.send_text(f'{athlete} belegt Platz {str(athlete_result.rank)} mit {result}.')
 
 
