9da1bf3786a21c64163cc3200095c87a0b95b6e5
Marcus Weiner <marcus.weiner@gmail.com>
2018-01-29 17:26:01 +0100

    :bug: Define SECRET_KEY in worker dashboard for django load

--- dashboard_production.sh
+++ dashboard_production.sh
@@ -4,5 +4,5 @@ MONGODB_URI=`heroku config:get MONGODB_URI`
 REDIS_URL=`heroku config:get REDIS_URL`
 
 docker-compose build worker_dashboard
-docker run -it -p 127.0.0.1:5556:5555 -e MONGODB_URI=${MONGODB_URI} -e REDIS_URL=${REDIS_URL} \
+docker run -it -p 127.0.0.1:5556:5555 -e MONGODB_URI=${MONGODB_URI} -e REDIS_URL=${REDIS_URL} -e SECRET_KEY=1 \
        sportsfreund_worker_dashboard mrq-dashboard
