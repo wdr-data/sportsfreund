3a3405d60e903a780e8c4019f136767096f8bc4c
LizzAy <lisachenbach@gmail.com>
2018-02-01 18:35:34 +0100

    :bug: Fix data structure

--- feeds/models/person.py
+++ feeds/models/person.py
@@ -8,3 +8,15 @@ class Person(FeedModel):
     api_function = api.person
     api_id_name = 'pe'
     cache_time = 60 * 60 * 24 * 7
+
+    @classmethod
+    def by_id(cls, id, topic_id, additional_data=None, clear_cache=False):
+        return super(Person, cls).by_id(id, clear_cache, {'to': topic_id}, additional_data)
+
+    @classmethod
+    def transform(cls, person):
+        sport = person['sport']
+        person = person['person']
+        person['sport'] = sport
+
+        return person
