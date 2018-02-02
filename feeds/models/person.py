ea43b825ba4499ee147c86fc74b8341fdbac80b0
Marcus Weiner <marcus.weiner@gmail.com>
2018-02-02 11:02:41 +0100

    :art: Provide method for counting persons in feeds

--- feeds/models/person.py
+++ feeds/models/person.py
@@ -1,6 +1,12 @@
+import logging
+
+from feeds.models.match import Match
+from feeds.models.match_meta import MatchMeta
 from .. import api
 from lib.mongodb import db
 from .model import FeedModel
+
+logger = logging.getLogger(__name__)
 
 
 class Person(FeedModel):
@@ -20,3 +26,35 @@ class Person(FeedModel):
         person['sport'] = sport
 
         return person
+
+    @classmethod
+    def count_in_feed(cls):
+        persons_full = set()
+        meta_matches = MatchMeta._search()
+        for meta_match in meta_matches:
+            try:
+                meta_match = MatchMeta(**meta_match)
+                if meta_match.get('id') is not None:
+                    logger.debug(f"Fetching match {meta_match.id}")
+                    match = Match.by_id(meta_match.id)
+
+                    if match.get('match_result') is None:
+                        continue
+
+                    for result in match.match_result:
+                        if result.get('person') is None:
+                            continue
+
+                        persons = result.person
+
+                        if not isinstance(persons, list):
+                            persons = [persons]
+
+                        for person in persons:
+                            persons_full.add(person.id)
+                    print(len(persons_full))
+            except Exception as err:
+                logger.debug(err)
+                continue
+
+        return len(persons_full)
