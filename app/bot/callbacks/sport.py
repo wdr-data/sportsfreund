8923220a744fec201d57e23e0da4dffac89a5d9d
LizzAy <lisachenbach@gmail.com>
2018-02-09 12:33:00 +0100

    :bug: Use correct payload to trigger function

--- app/bot/callbacks/sport.py
+++ app/bot/callbacks/sport.py
@@ -73,7 +73,8 @@ def api_sport(event, parameters,**kwargs):
             list_element(report.headline, report.text,
                          image_url=report.media.url if str(report.media) else None,
                          buttons=[button_postback('Lesen...',
-                                                  {'report_sport': sport})]))
+                                                  {'report_sport': sport,
+                                                   'report_discipline': None})]))
 
     if story:
         elements.append(
