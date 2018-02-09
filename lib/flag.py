39d55cc155c75824b61f14dc6dae3cbdacd9af4b
ChristianJoe <cjoe@gmx.de>
2018-02-09 10:24:50 +0100

    :bug: Show Flag for mixed team of korea in icehockey

--- lib/flag.py
+++ lib/flag.py
@@ -4,6 +4,8 @@ OFFSET = 127462 - ord('A')
 def flag(code):
 
     if code == '🏳️‍🌈':
-        return code
+        return "🏳️‍🌈"
+    if code == '':
+        return '🇰🇵🇰🇷'
 
     return chr(ord(code[0]) + OFFSET) + chr(ord(code[1]) + OFFSET)
