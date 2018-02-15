57826e57772bc0287403d40a75dfff76b1f6c4ce
Jannes Höke <jhoeke@uni-bremen.de>
2018-02-15 11:57:58 +0100

    :ambulance: Close redis connection

--- lib/redis.py
+++ lib/redis.py
@@ -1,12 +1,15 @@
+import contextlib
+
 import os
 import redis as redis_
 
 
 HANDLED_UPDATES_FB = 'handled_updates_fb'
+REDIS_URL = os.environ.get('REDIS_URL')
 
-try:
-    REDIS_URL = os.environ['REDIS_URL']
+
+@contextlib.contextmanager
+def redis_connection():
     redis = redis_.StrictRedis.from_url(REDIS_URL)
-
-except KeyError:
-    redis = None
+    yield redis
+    redis.connection_pool.disconnect()
